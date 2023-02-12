02b643bc2af11258dd5abfd9797f317fd607c2dc
Sergey <sokolovsergeysse@gmail.com>
2023-02-12 21:50:28 +0200

    create app

---
+++ src/components/emploees-list/emploees-list.js
@@ -0,0 +1,16 @@
+import EmploeesListItem from '../emploees-list-item/emploees-list-item';
+
+import './emploees-list.css';
+
+const EmploeesList = () => {
+  return (
+    <ul className="app-list list-group">
+      <EmploeesListItem/>
+      <EmploeesListItem/>
+      <EmploeesListItem/>
+    </ul>
+  )
+}
+
+
+export default EmploeesList;
